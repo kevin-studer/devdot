328ccf0b14b07f04189e570d1ce7004faecf0d82
kevin-studer <kevin.studer@unibe.ch>
2024-03-10 15:36:25 +0100

    Do not start installation

--- install.sh
+++ install.sh
@@ -2,18 +2,3 @@
 
 echo "=> Installing my dotfiles"
 
-if [ ! -f "/etc/debian_version" ]; then
-  echo "   Only works on debian based machines"
-	exit 1
-fi
-
-if [ ! -x "$(command -v hx)" ]; then
-  echo "   Installing editor"
-  sudo add-apt-repository -y ppa:maveonair/helix-editor
-  sudo apt update
-  sudo apt install -y helix 
-
-fi
-
-echo "=> Finished"
-
