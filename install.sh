ed78d1eeb09738a92f33e9d1d159b96128ba9994
kevin-studer <kevin.studer@unibe.ch>
2024-03-11 09:47:57 +0100

    Installing helix configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "=> Installing my dotfiles"
+set -e
 
+echo "=> Installing the dotfiles"
+
+if [ ! -d ~/.config ]; then
+  mkdir ~/.config
+fi
+
+if [ ! -d ~/.config/helix ]; then
+  mkdir ~/.config/helix
+fi
+
+stow -d . -t ~/.config/helix helix
